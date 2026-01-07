111571210065de32252ddf687ef09a5599ae7724
Ashok Ram <ak9000812219@gmail.com>
2026-01-07 11:44:24 +0530

    fix loader height

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,9 @@
 export default function Loading() {
   return (
-    <div className="loading-spinner">
-      <div className="spinner"></div>
+    <div style={{ minHeight: '100vh', position: 'relative' }}>
+      <div className="loading-spinner">
+        <div className="spinner"></div>
+      </div>
     </div>
   );
 }
